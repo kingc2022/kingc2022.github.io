a8934b213af560b41b24b7ebfffe4e800840c8b4
kingc2022 <kingcai_2020@126.com>
2022-05-07 21:25:05 +0800

    完成网站

---
+++ games/guess/js/guess.js
@@ -0,0 +1,58 @@
+var number = random(1, 101);
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function check() {
+    var input_number = document.getElementById("num").value;
+    var input_number_int = parseInt(input_number);
+    if (input_number_int >= 1 && input_number_int <= 100) {
+        if (input_number_int == number) {
+            $("#tishi").removeClass("btn-outline-danger");
+            $("#tishi").removeClass("btn-outline-primary");
+            $("#tishi").addClass("btn-outline-success");
+            $("#tishi").html("(*╹▽╹*) 猜对啦! (*╹▽╹*)");
+            $("#num").attr("disabled", "disabled");
+            $("#conf").attr("disabled", "disabled");
+            $("#rs").show();
+            console.clear();
+        } else if (input_number_int < number) {
+            $("#tishi").removeClass("btn-outline-danger");
+            $("#tishi").removeClass("btn-outline-primary");
+            $("#tishi").removeClass("btn-outline-success");
+            $("#tishi").addClass("btn-outline-danger");
+            $("#num").val("");
+            $("#tishi").html("(*╹▽╹*) 猜小啦! (*╹▽╹*)");
+            console.clear();
+        } else if (input_number_int > number) {
+            $("#tishi").removeClass("btn-outline-danger");
+            $("#tishi").removeClass("btn-outline-primary");
+            $("#tishi").removeClass("btn-outline-success");
+            $("#tishi").addClass("btn-outline-danger");
+            $("#num").val("");
+            $("#tishi").html("(*╹▽╹*) 猜大啦! (*╹▽╹*)");
+            console.clear();
+        }
+    } else {
+        $("#tishi").removeClass("btn-outline-primary");
+        $("#tishi").addClass("btn-outline-danger");
+        $("#num").val("");
+        $("#tishi").html("你的输入不合法");
+        console.clear();
+    }
+}
+
+function reset() {
+    number = random(1, 101);
+    $("#tishi").removeClass("btn-outline-danger");
+    $("#tishi").removeClass("btn-outline-primary");
+    $("#tishi").removeClass("btn-outline-success");
+    $("#tishi").addClass("btn-outline-primary");
+    $("#tishi").html("(*╹▽╹*) 提示区 (*╹▽╹*)");
+    $("#num").removeAttr("disabled");
+    $("#conf").removeAttr("disabled");
+    $("#num").val("");
+    $("#rs").hide();
+    console.clear();
+}
